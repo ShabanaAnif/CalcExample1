f3c3f5015ec4c9e5029e77a033d5be7b5d8adc52
shabana <J.maxwell@simplilearn.com>
2024-02-17 01:17:52 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CalcExample;
+Calc calc = new Calc();
+Console.WriteLine("Add Call");
+calc.Add(12, 45);
+Console.WriteLine("Multiply Call");
+calc.Multi(10.5, 12.45);
